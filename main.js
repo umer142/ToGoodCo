7bf840f95691c50c5009fd708bbccc050c8ad434
umer142 <umersjafiq79@gmail.com>
2025-02-04 02:38:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector(".menu");
+menu.addEventListener("click" , ()=>{
+    const menuBar =  document.querySelector(".menuBar");
+    menuBar.classList.toggle('hidden')
+    // menuBar.style.display = "block"
+    // const navbar = document.querySelector(".navbar")
+    // navbar.style.backgroundColor = "black"
+    // navbar.style.color = "white"
+    
+})
